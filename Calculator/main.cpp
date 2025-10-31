0e6d903f76e1dc2f17d5916ff48ecbb86d10510d
lostakey <malyshev0803@gmail.com>
2025-10-31 13:12:42 +0300

    fixed finding plugins

--- Calculator/main.cpp
+++ Calculator/main.cpp
@@ -1,13 +1,36 @@
 #include <iostream>
 #include <string>
+#include <filesystem>
 #include "pluginmanager.h"
 #include "calculator.h"
+
+namespace fs = std::filesystem;
+
+std::string findPluginsPath() {
+    std::vector<fs::path> possiblePaths = {
+        fs::current_path() / "plugins",
+        fs::current_path().parent_path() / "plugins",
+        fs::current_path() / ".." / "plugins",
+    };
+
+    for (const auto& path : possiblePaths) {
+        if (fs::exists(path) && fs::is_directory(path)) {
+            return path.string();
+        }
+    }
+
+    fs::create_directory("plugins");
+    return "plugins";
+}
 
 int main() {
     std::cout << "=== Advanced Calculator with Plugins ===\n";
 
     PluginManager pm;
-    pm.loadPlugins("./plugins");
+    std::string pluginsPath = findPluginsPath();
+    std::cout << "Looking for plugins in: " << pluginsPath << "\n";
+
+    pm.loadPlugins(pluginsPath);
 
     Calculator calc(&pm);
 
